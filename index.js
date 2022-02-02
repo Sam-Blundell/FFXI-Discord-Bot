6731922b3382ff3662f514b9ab0af75d51df543b
Sam Blundell <samcalblundell@googlemail.com>
2022-02-02 21:15:29 +0000

    add event listener in index file to listen for slash commands and reply, this will be moved to dedicated commandfiles in future

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('pong');
+	} else if (commandName === 'server') {
+		await interaction.reply('[placeholder for future server info]');
+	} else if (commandName === 'user') {
+		await interaction.reply('[placeholder for future user info');
+	}
+});
+
 client.login(token);
