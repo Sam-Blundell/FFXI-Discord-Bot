12b2430575cd0a3cd30d26b25cfc578cf7c6a4bb
Sam Blundell <samcalblundell@googlemail.com>
2022-02-04 00:46:36 +0000

    create event handler to assign newly joined users the newbie role

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,14 @@
 module.exports = {
 	name: 'guildMemberAdd',
-	execute(e) {
-		console.log('here');
-		console.log(e);
+	async execute(interaction) {
+		console.log(interaction);
+		
+		await interaction.guild.channels.cache.get('938532408779743276')
+			.send('Welcome ' + interaction.user.username);
+
+		const newbieRole = await interaction.guild.roles.cache
+			.get('938950302902652968');
+
+		await interaction.roles.add(newbieRole);
 	}
 };
