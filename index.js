39a8a5cf59c9f623fe370c82d81dcbb86e73f9e1
Sam Blundell <samcalblundell@googlemail.com>
2022-02-04 00:47:23 +0000

    fix intents on client object

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { token } = require('./config.json');
 const client = new Client({
 	intents: [
 		Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_PRESENCES,
-		// Intents.FLAGS.GUILD_MEMBERS,
+		// Intents.FLAGS.GUILD_PRESENCES,
+		Intents.FLAGS.GUILD_MEMBERS,
 		// Intents.FLAGS.GUILD_MESSAGES
 	] 
 });
