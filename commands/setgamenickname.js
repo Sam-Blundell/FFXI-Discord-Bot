c4be2eca1abb9fd49f87203d6bc170947f732c42
Sam Blundell <samcalblundell@googlemail.com>
2022-02-05 03:57:10 +0000

    complete setgamenickname

--- commands/setgamenickname.js
+++ commands/setgamenickname.js
@@ -1,3 +1,4 @@
+const getCharacter = require('../api.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
@@ -6,15 +7,32 @@ module.exports = {
 		.setDescription('Checks lodestone and sets your discord nick')
 		.addStringOption(option => option.setName('nickname')
 			.setDescription('your in-game username')
+			.setRequired(true))
+		.addStringOption(option => option.setName('server')
+			.setDescription('the server your character is on')
 			.setRequired(true)),
 	async execute(interaction) {
 
 		const newNick = interaction.options.getString('nickname');
+		const server = interaction.options.getString('server');
 
-		await interaction.member.setNickname(newNick);
+		await getCharacter(newNick, server)
+			.then((data) => {
+				console.log(data);
+				return data;
+			})
+			.then((data) => {
+				if (!data) {
+					interaction.reply(
+						`The character ${newNick} doesn't exist on ${server}`
+					);
+				} else {
+					interaction.reply(
+						`Setting your nickname to ${newNick}...`
+					);
+					interaction.member.setNickname(newNick);
+				}
+			});
 
-		await interaction.reply(
-			`Setting your nickname to ${newNick}...`
-		);
 	}
 };
