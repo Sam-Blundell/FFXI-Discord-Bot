6e922df90401241dcafd9fcdcffe14128560cf7d
Sam Blundell <samcalblundell@googlemail.com>
2022-02-05 02:36:40 +0000

    configure setnick command to take a parameter

--- commands/setgamenickname.js
+++ commands/setgamenickname.js
@@ -3,13 +3,18 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('setgamenickname')
-		.setDescription('Checks lodestone and sets your discord nick'),
+		.setDescription('Checks lodestone and sets your discord nick')
+		.addStringOption(option => option.setName('nickname')
+			.setDescription('your in-game username')
+			.setRequired(true)),
 	async execute(interaction) {
 
-		await interaction.member.setNickname('newnickname');
+		const newNick = interaction.options.getString('nickname');
+
+		await interaction.member.setNickname(newNick);
 
 		await interaction.reply(
-			'setting your nick...'
+			`Setting your nickname to ${newNick}...`
 		);
 	}
 };
