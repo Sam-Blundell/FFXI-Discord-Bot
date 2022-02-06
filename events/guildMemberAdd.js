d82c17ae66b6ab5177e1b6ab68f07412c4012a7b
Sam Blundell <samcalblundell@googlemail.com>
2022-02-06 01:03:32 +0000

    refactor guildMemberAdd to use embed formatting and write a description

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,13 +1,28 @@
 const { newbie } = require('../roles.json');
 const { newbiechannel } = require('../channels.json');
+const { MessageEmbed } = require('discord.js');
+
+const dscr1 = 'Please use the /name command to set your server ';
+const dscr2 = 'nickname to be the same as your in-game name. The command ';
+const dscr3 = 'uses two parameters, name & server.\nFor example:\n';
+const dscr4 = '/name [Adrian Adelbert] [Ravana]';
 
 module.exports = {
 	name: 'guildMemberAdd',
 	async execute(interaction) {
-		console.log(interaction);
-		
+
+		const newMemberJoin = new MessageEmbed()
+			.setColor('#02d642')
+			.setTitle(`Hi ${interaction.user.username}! Welcome to the server.`)
+			.setAuthor({
+				name: 'Augur',
+				iconURL: 'https://i.imgur.com/KpGs20S.jpeg'
+			})
+			.setDescription(dscr1 + dscr2 + dscr3 + dscr4)
+			.setTimestamp();
+
 		await interaction.guild.channels.cache.get(newbiechannel)
-			.send('Welcome ' + interaction.user.username);
+			.send({ embeds: [newMemberJoin] });
 
 		const newbieRole = await interaction.guild.roles.cache
 			.get(newbie);
