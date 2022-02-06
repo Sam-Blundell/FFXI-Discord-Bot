fe06e5348b1b8e773d71e84445d0e9e25e947635
Sam Blundell <samcalblundell@googlemail.com>
2022-02-06 23:20:57 +0000

    create event listener for when a guild member is updated, this will spawn role choice menu if they have the newbie role

---
+++ events/guildMemberUpdate.js
@@ -0,0 +1,37 @@
+const { newbie } = require('../roles.json');
+const { newbiechannel } = require('../channels.json');
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+
+module.exports = {
+	name: 'guildMemberUpdate',
+	async execute(oldMember, newMember) {
+
+		const usrNm = newMember.nickname;
+
+		const memberMsg = `Thanks ${usrNm}, `;
+		const memberMsg2 = 'so are you a member of Omen or just a friend?';
+		
+		if (!newMember.roles.cache.has(newbie) || newMember.nickname === null) {
+			return;
+		} else {
+			const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId('memberrole')
+						.setLabel('Member')
+						.setStyle('PRIMARY'),
+					new MessageButton()
+						.setCustomId('friendrole')
+						.setLabel('Friend')
+						.setStyle('SUCCESS')
+				);
+			
+			await newMember.guild.channels.cache.get(newbiechannel)
+				.send({
+					content: memberMsg + memberMsg2,
+					components: [row]
+				});
+		}
+	}
+};
