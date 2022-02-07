ab876244fd58c8af0816589ec349dd18cce655bc
Sam Blundell <samcalblundell@googlemail.com>
2022-02-07 01:06:51 +0000

    add logic to generic interaction handler to handle button presses in the role picker

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,10 +1,43 @@
+const { newbie, member, friend } = require('../roles.json');
+const { generalchannel } = require('../channels.json');
+
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
 		const {
 			user: { tag = 'usertagnotfound' },
-			channel: { name = 'channelnotfound' }
+			channel: { name = 'channelnotfound' },
 		} = interaction;
 		console.log(`${tag} in #${name} triggered an interaction`);
+
+		if (!interaction.isButton()) {
+			return;
+		}
+
+		const { customId, member: { nickname } } = interaction;
+		
+
+		if (
+			(customId === 'memberrole' || customId === 'friendrole')
+			&& interaction.member.roles.cache.has(newbie)
+		) {
+			if (customId === 'memberrole') {
+				interaction.member.roles.remove(newbie);
+				interaction.member.roles.add(member);
+
+				interaction.guild.channels.cache.get(generalchannel)
+					.send({
+						content: `${nickname} has joined as a new member.`
+					});
+			} else if (customId === 'friendrole') {
+				interaction.member.roles.remove(newbie);
+				interaction.member.roles.add(friend);
+
+				interaction.guild.channels.cache.get(generalchannel)
+					.send({
+						content: `${nickname} has joined as a new friend.`
+					});
+			}
+		}
 	}
 };
