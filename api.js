3aa87b17252109d8a80ce93d1ace4477e6c2b195
Sam Blundell <samcalblundell@googlemail.com>
2022-07-18 20:50:16 +0100

    fix lack of return value on getCharacter function and refactor

--- api.js
+++ api.js
@@ -5,16 +5,14 @@ const xivApi = axios.create({
     baseURL: 'https://xivapi.com/',
 });
 
-const getCharacter = (name, server) => {
-    xivApi.get('character/search', {
-        params: {
-            // eslint-disable-next-line camelcase
-            private_key: xivApiToken,
-            name,
-            server,
-        },
-    })
-        .then(({ data }) => data.Results[0]);
-};
+const getCharacter = (name, server) => xivApi.get('character/search', {
+    params: {
+        // eslint-disable-next-line camelcase
+        private_key: xivApiToken,
+        name,
+        server,
+    },
+})
+    .then(({ data }) => data.Results[0]);
 
 export { getCharacter };
